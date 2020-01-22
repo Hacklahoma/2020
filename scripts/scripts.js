4643c9c4164f252636ba8f3c2683b9e73ced5680
s-fletcher <sfletchertx@gmail.com>
2020-01-21 18:55:58 -0600

    added warning

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -205,35 +205,35 @@ function floorPrev() {
 var countDownDate = new Date("Jan 24, 2020 23:59:59").getTime();
 
 // Update the count down every 1 second
-  var x = setInterval(function () {
-    // Get today's date and time
-    var now = new Date().getTime();
+  // var x = setInterval(function () {
+  //   // Get today's date and time
+  //   var now = new Date().getTime();
 
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
+  //   // Find the distance between now and the count down date
+  //   var distance = countDownDate - now;
 
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  //   // Time calculations for days, hours, minutes and seconds
+  //   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  //   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  //   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  //   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    try {
-      // Display the result in the element with id="demo"
-      document.getElementById("countdown").innerHTML = "Registration closes in " + days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s!";
-    } catch {}
-    // If the count down is finished, write some text
-    if (distance < 0) {
-      clearInterval(x);
-      try {
-        document.getElementById("countdown").innerHTML = "Registration is closed!";
-        document.getElementById("registerButton").innerHTML = "Day of Info";
-        document.getElementById("buttonHref").href = "./live";
-        document.getElementById("learnMore").innerHTML = "";
-      } catch {}
-    }
-  }, 1000);
+  //   try {
+  //     // Display the result in the element with id="demo"
+  //     document.getElementById("countdown").innerHTML = "Registration closes in " + days + "d " + hours + "h "
+  //       + minutes + "m " + seconds + "s!";
+  //   } catch {}
+  //   // If the count down is finished, write some text
+  //   if (distance < 0) {
+  //     clearInterval(x);
+  //     try {
+  //       document.getElementById("countdown").innerHTML = "Registration is closed!";
+  //       document.getElementById("registerButton").innerHTML = "Day of Info";
+  //       document.getElementById("buttonHref").href = "./live";
+  //       document.getElementById("learnMore").innerHTML = "";
+  //     } catch {}
+  //   }
+  // }, 1000);
 /* scroll reveal */
 AOS.init({
   once: true, // whether animation should happen only once - while scrolling down
