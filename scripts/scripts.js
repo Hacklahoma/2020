5d2f4b3f9c56fa1ea96df936cadd5460f342717a
s-fletcher <sfletchertx@gmail.com>
2020-02-02 13:05:44 -0600

    Added link to location

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -201,6 +201,17 @@ function floorPrev() {
   }
 }
 
+// Controlling map selector
+function mapsSelector() {
+  if /* if we're on iOS, open in Apple Maps */
+    ((navigator.platform.indexOf("iPhone") != -1) ||
+    (navigator.platform.indexOf("iPad") != -1) ||
+    (navigator.platform.indexOf("iPod") != -1))
+    window.open("maps://maps.google.com/maps?daddr=35.210731,-97.441816&amp;ll=");
+  else /* else use Google */
+    window.open("https://maps.google.com/maps?daddr=35.210731,-97.441816&amp;ll=");
+}
+
 // /* countdown */
 // var countDownDate = new Date("Jan 29, 2020 23:59:59").getTime();
 
